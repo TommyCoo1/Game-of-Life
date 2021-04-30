35eb22d274db0707253af592896de2249ff67e70
Reisliebhaber <pmntt21@gmail.com>
2021-04-30 11:47:55 +0200

    add Start/Stop button & builds Game Field, when program is executed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace GameOfLife
 {
@@ -23,19 +24,122 @@ namespace GameOfLife
         public MainWindow()
         {
             InitializeComponent();
+
+            Random würfel = new Random();
+
+            spielfeld.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            spielfeld.Arrange(new Rect(0.0, 0.0, spielfeld.DesiredSize.Width, spielfeld.DesiredSize.Height));
+
+            for (int height = 0; height < CellNumberWidth; height++)
+            {
+                for (int width = 0; width < CellNumberHeight; width++)
+                {
+                    Rectangle r = new Rectangle();
+                    r.Width = (spielfeld.ActualWidth / CellNumberWidth) - 1.0;
+                    r.Height = (spielfeld.ActualHeight / CellNumberHeight) - 1.0;
+                    r.Fill = Brushes.MediumAquamarine;
+                    spielfeld.Children.Add(r);
+                    Canvas.SetLeft(r, width * spielfeld.ActualWidth / CellNumberWidth);
+                    Canvas.SetTop(r, height * spielfeld.ActualHeight / CellNumberHeight);
+                    r.MouseDown += R_MouseDown;
+
+                    Felder[height, width] = r;
+                }
+            }
+
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            {
+                int[,] anzahlNachbarn = new int[CellNumberHeight, CellNumberWidth]; //Array für jede Zelle/Rechteck Nachbarn merken ==> Zelle - Anzahlnachbarn
+
+                for (int height = 0; height < CellNumberWidth; height++)
+                {
+                    for (int width = 0; width < CellNumberHeight; width++)
+                    {
+                        int topCell = height - 1;
+                        if (topCell < 0)
+                            topCell = CellNumberHeight - 1;
+                        int botCell = height + 1;
+                        if (botCell >= CellNumberHeight)
+                            botCell = 0;
+                        int leftCell = width - 1;
+                        if (leftCell < 0)
+                            leftCell = CellNumberWidth - 1;
+                        int rightCell = width + 1;
+                        if (rightCell >= CellNumberWidth)
+                            rightCell = 0;
+
+                        int nachbarn = 0;
+
+                        if (Felder[topCell, leftCell].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+                        if (Felder[topCell, width].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+                        if (Felder[topCell, rightCell].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+                        if (Felder[height, leftCell].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+                        if (Felder[height, rightCell].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+                        if (Felder[botCell, leftCell].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+                        if (Felder[botCell, width].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+                        if (Felder[botCell, rightCell].Fill == Brushes.DeepPink)
+                        { nachbarn++; }
+
+                        anzahlNachbarn[height, width] = nachbarn;
+
+
+                    }
+                }
+
+                for (int height = 0; height < CellNumberWidth; height++)
+                {
+                    for (int width = 0; width < CellNumberHeight; width++)
+                    {
+                        if (anzahlNachbarn[height, width] < 2 || anzahlNachbarn[height, width] > 3)
+                        {
+                            Felder[height, width].Fill = Brushes.MediumAquamarine;
+                        }
+                        else if (anzahlNachbarn[height, width] == 3)
+                        {
+                            Felder[height, width].Fill = Brushes.DeepPink;
+                        }
+                        // Felder[height, width].Fill = (anzahlNachbarn[height, width] < 2 || anzahlNachbarn[height, width] > 3)  ? Brushes.Cyan : (anzahlNachbarn[height, width] == 3) ? Felder[height, width].Fill = Brushes.Red : ---;
+                    }
+                }
+            }
         }
 
         /*public int CellNumberWidth { get; set; } = 30;
           public int CellNumberHeight { get; set; } = 30;
           Rectangle[,] Felder => new Rectangle[CellNumberWidth, CellNumberHeight]; */
-        const int CellNumberWidth= 30;
+        const int CellNumberWidth = 30;
         const int CellNumberHeight = 30;
+        DispatcherTimer timer = new DispatcherTimer();
+
         Rectangle[,] Felder = new Rectangle[CellNumberWidth, CellNumberHeight];
-        public int test = 3;
-        Rectangle[] teest = new Rectangle[3];
+        List<List<Rectangle>> Felder2 = new List<List<Rectangle>>();
+
+        public static List<List<Rectangle>> adjustList(List<List<Rectangle>> list, int numberoflists)
+        {
+            for (int i = 0; i < numberoflists; i++)
+            {
+                list.Add(new List<Rectangle>());
+            }
+            return list;
+        }
+
 
         private void start_button_Click(object sender, RoutedEventArgs e)
         {
+            Felder2 = adjustList(Felder2, CellNumberWidth);
 
             for (int height = 0; height < CellNumberWidth; height++)
             {
@@ -123,5 +227,21 @@ namespace GameOfLife
                 }
             }
         }
+
+        private void start_stop_button(object sender, RoutedEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                timer.Stop();
+                startstop.Content = "Starte Animation!";
+            }
+            else
+            {
+                timer.Start();
+                startstop.Content = "Stoppe Animation!";
+            }
+        }
+
+
     }
 }
