508a6dff0d70f3a333a309b6cb00718932162556
Reisliebhaber <pmntt21@gmail.com>
2021-04-30 21:18:01 +0200

    added Random Button to place random living cells

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,11 +21,19 @@ namespace GameOfLife
     /// </summary>
     public partial class MainWindow : Window
     {
+        /*public int CellNumberWidth { get; set; } = 30;
+          public int CellNumberHeight { get; set; } = 30;
+          Rectangle[,] Felder => new Rectangle[CellNumberWidth, CellNumberHeight]; */
+        const int CellNumberWidth = 30;
+        const int CellNumberHeight = 30;
+        DispatcherTimer timer = new DispatcherTimer();
+
+        Rectangle[,] Felder = new Rectangle[CellNumberWidth, CellNumberHeight];
+        List<List<Rectangle>> Felder2 = new List<List<Rectangle>>();
+
         public MainWindow()
         {
             InitializeComponent();
-
-            Random würfel = new Random();
 
             spielfeld.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
             spielfeld.Arrange(new Rect(0.0, 0.0, spielfeld.DesiredSize.Width, spielfeld.DesiredSize.Height));
@@ -54,78 +62,8 @@ namespace GameOfLife
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            {
-                int[,] anzahlNachbarn = new int[CellNumberHeight, CellNumberWidth]; //Array für jede Zelle/Rechteck Nachbarn merken ==> Zelle - Anzahlnachbarn
-
-                for (int height = 0; height < CellNumberWidth; height++)
-                {
-                    for (int width = 0; width < CellNumberHeight; width++)
-                    {
-                        int topCell = height - 1;
-                        if (topCell < 0)
-                            topCell = CellNumberHeight - 1;
-                        int botCell = height + 1;
-                        if (botCell >= CellNumberHeight)
-                            botCell = 0;
-                        int leftCell = width - 1;
-                        if (leftCell < 0)
-                            leftCell = CellNumberWidth - 1;
-                        int rightCell = width + 1;
-                        if (rightCell >= CellNumberWidth)
-                            rightCell = 0;
-
-                        int nachbarn = 0;
-
-                        if (Felder[topCell, leftCell].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-                        if (Felder[topCell, width].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-                        if (Felder[topCell, rightCell].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-                        if (Felder[height, leftCell].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-                        if (Felder[height, rightCell].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-                        if (Felder[botCell, leftCell].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-                        if (Felder[botCell, width].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-                        if (Felder[botCell, rightCell].Fill == Brushes.DeepPink)
-                        { nachbarn++; }
-
-                        anzahlNachbarn[height, width] = nachbarn;
-
-
-                    }
-                }
-
-                for (int height = 0; height < CellNumberWidth; height++)
-                {
-                    for (int width = 0; width < CellNumberHeight; width++)
-                    {
-                        if (anzahlNachbarn[height, width] < 2 || anzahlNachbarn[height, width] > 3)
-                        {
-                            Felder[height, width].Fill = Brushes.MediumAquamarine;
-                        }
-                        else if (anzahlNachbarn[height, width] == 3)
-                        {
-                            Felder[height, width].Fill = Brushes.DeepPink;
-                        }
-                        // Felder[height, width].Fill = (anzahlNachbarn[height, width] < 2 || anzahlNachbarn[height, width] > 3)  ? Brushes.Cyan : (anzahlNachbarn[height, width] == 3) ? Felder[height, width].Fill = Brushes.Red : ---;
-                    }
-                }
-            }
+            updateCells();
         }
-
-        /*public int CellNumberWidth { get; set; } = 30;
-          public int CellNumberHeight { get; set; } = 30;
-          Rectangle[,] Felder => new Rectangle[CellNumberWidth, CellNumberHeight]; */
-        const int CellNumberWidth = 30;
-        const int CellNumberHeight = 30;
-        DispatcherTimer timer = new DispatcherTimer();
-
-        Rectangle[,] Felder = new Rectangle[CellNumberWidth, CellNumberHeight];
-        List<List<Rectangle>> Felder2 = new List<List<Rectangle>>();
 
         public static List<List<Rectangle>> adjustList(List<List<Rectangle>> list, int numberoflists)
         {
@@ -166,6 +104,11 @@ namespace GameOfLife
         }
 
         private void Button_Naester_Schritt_Click(object sender, RoutedEventArgs e)
+        {
+            updateCells();
+        }
+
+        private void updateCells()
         {
             int[,] anzahlNachbarn = new int[CellNumberHeight, CellNumberWidth]; //Array für jede Zelle/Rechteck Nachbarn merken ==> Zelle - Anzahlnachbarn
 
@@ -242,6 +185,18 @@ namespace GameOfLife
             }
         }
 
+        private void random_button(object sender, RoutedEventArgs e)
+        {
 
+            Random random = new Random();
+
+            for (int height = 0; height < CellNumberWidth; height++)
+            {
+                for (int width = 0; width < CellNumberHeight; width++)
+                {
+                    Felder[height, width].Fill = (random.Next(0, 2) == 1) ? Brushes.MediumAquamarine : Brushes.DeepPink;
+                }
+            }
+        }
     }
 }
